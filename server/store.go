4187e4d296c37cafa89a1d33d0e32beda1594e1c
ming <minghsu0107@gmail.com>
2023-11-13 12:11:50 -0800

    generalize store

--- server/store.go
+++ server/store.go
@@ -20,29 +20,33 @@ func NewStore(conn *nats.Conn, kv jetstream.KeyValue) *Store {
 	return &Store{conn, kv}
 }
 
-func (s *Store) GetFmDevice(ctx context.Context, deviceName string) (bool, *common.FMDevice, error) {
-	entry, err := s.kv.Get(ctx, common.KVStoreKey(common.FM, deviceName))
+func (s *Store) GetDevice(ctx context.Context, sdrType common.SDRType, deviceName string) (bool, common.Device, error) {
+	entry, err := s.kv.Get(ctx, common.KVStoreKey(sdrType, deviceName))
 	if err != nil {
 		if errors.Is(err, jetstream.ErrKeyNotFound) {
 			return false, nil, nil
 		}
 		return false, nil, err
 	}
-	var device common.FMDevice
-	if err = json.Unmarshal(entry.Value(), &device); err != nil {
-		return false, nil, err
+	switch sdrType {
+	case common.FM:
+		var device common.FMDevice
+		if err = json.Unmarshal(entry.Value(), &device); err != nil {
+			return false, nil, err
+		}
+		return true, &device, nil
 	}
-	return true, &device, nil
+	return false, nil, nil
 }
 
-func (s *Store) GetFmDevices(ctx context.Context) ([]*common.FMDevice, error) {
+func (s *Store) GetDevices(ctx context.Context, sdrType common.SDRType) ([]common.Device, error) {
 	keys, err := s.kv.Keys(ctx)
 	if err != nil {
 		return nil, err
 	}
-	devices := []*common.FMDevice{}
+	devices := []common.Device{}
 	for _, key := range keys {
-		exist, device, err := s.GetFmDevice(ctx, common.DeviceNameFromKey(key))
+		exist, device, err := s.GetDevice(ctx, sdrType, common.DeviceNameFromKey(key))
 		if err != nil {
 			return nil, err
 		}
@@ -53,20 +57,20 @@ func (s *Store) GetFmDevices(ctx context.Context) ([]*common.FMDevice, error) {
 	return devices, nil
 }
 
-func (s *Store) UpdateFmDevice(ctx context.Context, fmDevice *common.FMDevice) error {
-	deviceInfoBytes, err := json.Marshal(fmDevice)
+func (s *Store) UpdateDevice(ctx context.Context, sdrType common.SDRType, deviceName string, deviceInfo common.Device) error {
+	deviceInfoBytes, err := json.Marshal(deviceInfo)
 	if err != nil {
 		return err
 	}
-	if _, err = s.kv.Put(ctx, common.KVStoreKey(common.FM, fmDevice.Name), deviceInfoBytes); err != nil {
+	if _, err = s.kv.Put(ctx, common.KVStoreKey(sdrType, deviceName), deviceInfoBytes); err != nil {
 		return err
 	}
-	_, err = s.conn.Request(common.ClusterSubject(common.FM, fmDevice.Name, common.WatchConfigCmd), nil, 3*time.Second)
+	_, err = s.conn.Request(common.ClusterSubject(sdrType, deviceName, common.WatchConfigCmd), nil, 3*time.Second)
 	return err
 }
 
-func (s *Store) DeleteFmDevice(ctx context.Context, deviceName string) error {
-	if err := s.kv.Delete(ctx, common.KVStoreKey(common.FM, deviceName)); err != nil {
+func (s *Store) DeleteDevice(ctx context.Context, sdrType common.SDRType, deviceName string) error {
+	if err := s.kv.Delete(ctx, common.KVStoreKey(sdrType, deviceName)); err != nil {
 		return err
 	}
 	return nil
