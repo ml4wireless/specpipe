092f24d2340ec0dc6c9c386783207c93c6675b23
William Almy <walmy@ucsc.edu>
2024-04-24 19:24:14 -0700

    Make speech2text demo configurable

--- _examples/speech-to-text-ws/main.py
+++ _examples/speech-to-text-ws/main.py
@@ -1,56 +1,79 @@
 import asyncio
 import os
+import random
+import string
 from collections import deque
 from io import BytesIO
 
-from fastapi import FastAPI, WebSocket
-from nats.aio.client import Client as NATS
 import speech_recognition as sr
+import uvicorn
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from nats.aio.client import Client as NATS
 
 app = FastAPI()
+
+NATS_URL = os.getenv("NATS_URL", "nats://127.0.0.1:4222")
+NATS_TOKEN = os.getenv("NATS_TOKEN", "mytoken")
+
+FM_TOPIC_PREFIX = "specpipe.data.fm"
+CHUNK_SIZE_SECONDS = 2
 
 
 @app.get("/")
 async def root():
+    """Standard liveness check"""
     return {"message": "OK"}
 
-@app.websocket("/text_random")
-async def text_random(websocket: WebSocket):
+
+@app.websocket("/ws/text_random")
+async def ws_text_random(websocket: WebSocket):
+    """Returns a random string of characters every second"""
     await websocket.accept()
-    while True:
-        random_bytes = str(os.urandom(10))
-        await websocket.send_text(str(random_bytes))
-        await asyncio.sleep(1)
+    try:
+        while True:
+            random_text = " ".join(random.choices(string.ascii_letters, k=40))
+            await websocket.send_text(random_text)
+            await asyncio.sleep(1)
+    except WebSocketDisconnect:
+        pass
+
 
 def create_wav_header(sample_rate, bits_per_sample, channels, num_samples):
-    """
-    Create a WAV file header for a given set of audio properties, 
-    assuming linear PCM audio.
-    """
+    """Returns WAV file header in bytes for a given audio configuration"""
     datasize = num_samples * channels * (bits_per_sample // 8)
-    o = bytes("RIFF",'ascii')                                                   # (4byte) Marks file as RIFF
-    o += (datasize + 36).to_bytes(4, 'little')                                  # (4byte) File size in bytes excluding this and RIFF marker
-    o += bytes("WAVE",'ascii')                                                  # (4byte) File type
-    o += bytes("fmt ",'ascii')                                                  # (4byte) Format Chunk Marker
-    o += (16).to_bytes(4, 'little')                                             # (4byte) Length of above format data
-    o += (1).to_bytes(2, 'little')                                              # (2byte) Format type (1 - PCM)
-    o += (channels).to_bytes(2, 'little')                                       # (2byte)
-    o += (sample_rate).to_bytes(4, 'little')                                    # (4byte)
-    o += (sample_rate * channels * (bits_per_sample // 8)).to_bytes(4, 'little')# (4byte)
-    o += (channels * (bits_per_sample // 8)).to_bytes(2, 'little')              # (2byte)
-    o += (bits_per_sample).to_bytes(2, 'little')                                # (2byte)
-    o += bytes("data",'ascii')                                                  # (4byte) Data Chunk Marker
-    o += (datasize).to_bytes(4, 'little')                                       # (4byte) Data size in bytes
+    o = bytes("RIFF", "ascii")  # (4byte) Marks file as RIFF
+    o += (datasize + 36).to_bytes(4, "little")  # (4byte) File size in bytes
+    o += bytes("WAVE", "ascii")  # (4byte) File type
+    o += bytes("fmt ", "ascii")  # (4byte) Format Chunk Marker
+    o += (16).to_bytes(4, "little")  # (4byte) Length of above format data
+    o += (1).to_bytes(2, "little")  # (2byte) Format type (1 - PCM)
+    o += (channels).to_bytes(2, "little")  # (2byte)
+    o += (sample_rate).to_bytes(4, "little")  # (4byte)
+    o += (sample_rate * channels * (bits_per_sample // 8)).to_bytes(4, "little")
+    o += (channels * (bits_per_sample // 8)).to_bytes(2, "little")  # (2byte)
+    o += (bits_per_sample).to_bytes(2, "little")  # (2byte)
+    o += bytes("data", "ascii")  # (4byte) Data Chunk Marker
+    o += (datasize).to_bytes(4, "little")  # (4byte) Data size in bytes
     return o
 
-def prepend_wav_header(audio_data, sample_rate=32000, bits_per_sample=16, channels=1):
+
+def prepend_wav_header(
+    audio_data: bytes,
+    sample_rate: int = 64000,
+    bits_per_sample: int = 16,
+    channels: int = 1,
+):
+    """Take raw audio data and prepend a WAV header to it, returning bytes"""
     num_samples = len(audio_data) // (channels * (bits_per_sample // 8))
     header = create_wav_header(sample_rate, bits_per_sample, channels, num_samples)
     return header + audio_data
 
-async def async_speech_to_text(audio_data):
-    audio_data_with_header = prepend_wav_header(audio_data)
+
+async def async_speech_to_text(audio_data: bytes, sample_rate: int):
+    """Given a chunk of bytes, return the transcribed text using the Sphinx engine"""
+    audio_data_with_header = prepend_wav_header(audio_data, sample_rate=sample_rate)
     recognizer = sr.Recognizer()
+
     audio_file = BytesIO(audio_data_with_header)
     with sr.AudioFile(audio_file) as source:
         audio = recognizer.record(source)
@@ -59,48 +82,55 @@ async def async_speech_to_text(audio_data):
     return text
 
 
-@app.websocket("/ws_fm_speech")
-async def ws_fm(websocket: WebSocket):
+@app.websocket("/ws/fm_speech/{device_id}/{sample_rate}")
+async def ws_fm_speech(
+    websocket: WebSocket, device_id: str = "dev0-mock", sample_rate: int = 64000
+):
+    """Websocket endpoint that listens to audio data from a device and streams transcribed text"""
     await websocket.accept()
 
     nc = NATS()
-    await nc.connect("nats://127.0.0.1:4222", token="mytoken")
+    await nc.connect(NATS_URL, token=NATS_TOKEN)
     js = nc.jetstream()
 
     error_event = asyncio.Event()
-
     audio_buffer = deque()
 
     async def message_handler(msg):
         audio_buffer.extend(msg.data)
-        if len(audio_buffer) >= 256000:
+        if len(audio_buffer) >= sample_rate * CHUNK_SIZE_SECONDS:
             try:
-                chunk_to_process = bytes([audio_buffer.popleft() for _ in range(256000)])
-                transcribed_text = await async_speech_to_text(chunk_to_process)
+                chunk_to_process = bytes(
+                    [
+                        audio_buffer.popleft()
+                        for _ in range(sample_rate * CHUNK_SIZE_SECONDS)
+                    ]
+                )
+                transcribed_text = await async_speech_to_text(
+                    chunk_to_process, sample_rate
+                )
                 await websocket.send_text(transcribed_text)
+            except WebSocketDisconnect as e:
+                error_event.set()
             except Exception as e:
                 print(str(e))
                 error_event.set()
 
-    sub = await js.subscribe("specpipe.data.fm.dev1", cb=message_handler)
+    sub = await js.subscribe(f"{FM_TOPIC_PREFIX}.{device_id}", cb=message_handler)
 
     try:
         while not error_event.is_set():
             await asyncio.sleep(1)
     except Exception as e:
         print(f"An error occurred in main loop: {e}")
-    
+
     await sub.unsubscribe()
     await nc.close()
-
-    try:
-      await websocket.close()
-    except RuntimeError:
-        pass
-
+    await websocket.close()
 
 
 if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    try:
+        uvicorn.run(app, host="0.0.0.0", port=8000)
+    except KeyboardInterrupt:
+        pass
